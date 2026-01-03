b9140536394d541e47b4d86ddba20ce844cea160
dev2kod <devesh4coding@gmail.com>
2026-01-03 14:44:59 +0530

    first commit

---
+++ ExpressClass/index.js
@@ -0,0 +1,46 @@
+const express = require("express")
+
+const app = express();
+app.use(express.json())
+
+const users = [{
+    name:"dev",
+    kidney:[{
+        healthy: false
+    },{
+        healthy:false
+    }]
+}];
+
+
+app.get("/",(req,res)=>{
+    const johnkidneys = users[0].kidney;
+    const numberofkidney = johnkidneys.length  
+    res.json({
+        "numberofkidney":numberofkidney
+    })
+})
+
+app.post("/add",(req,res)=>{
+    const data = req.body.healthy;
+    users[0].kidney.push({
+        healthy: data
+    })
+    res.json(users)})
+
+app.put("/treat",(req,res)=>{
+    for(let i=0;i<users[0].kidney.length;i++){
+        if(users[0].kidney[i].healthy===false){
+    users[0].kidney[i].healthy = true;
+            console.log("kidney repaired");
+        }
+        else{console.log("kidney healthy");}
+    }
+    res.json({
+        msg:"checked and repaired",
+        kidney: users[0].kidney,
+    })
+})
+
+app.listen(3000,()=>{console.log("running on 3000");
+})
