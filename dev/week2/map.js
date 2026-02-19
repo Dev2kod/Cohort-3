859679bf0a60a72bb1afd070c75a06f84e44cda6
Dev2kod <devesh4coding@gmail.com>
2026-02-19 08:24:05 +0530

    starting with the promises class... updating previous changes

--- dev/week2/map.js
+++ dev/week2/map.js
@@ -5,3 +5,15 @@ const add1 = (i) =>{return i+1};
 const op  = ip.map(add1);
 
 console.log(op);
+
+
+// filtering
+const input = [1,2,3,4,5,6,7,8,9,10];
+const output = input.filter((el)=>{
+    if(el%2==0){
+        return true;
+    }
+    else{return false}
+})
+
+console.log(output);
