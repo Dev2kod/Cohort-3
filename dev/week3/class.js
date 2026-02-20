41eb626981adc3bff07cfc2ee53b091dae1d6ec6
dev2kod <devesh4coding@gmail.com>
2026-02-20 23:56:38 +0530

    resolving conflicts

--- dev/week3/class.js
+++ dev/week3/class.js
@@ -7,3 +7,26 @@ const user = {
 }
 
 console.log(user.name);
+
+//classes and constructor
+
+class rectangle{
+    constructor(length,width,color){
+        this.length = length
+        this.width = width
+        this.color = color
+    }
+
+    area(){
+        const area = this.length * this.width
+        return area;
+    }
+    paint(){
+        console.log(`coloring the rectangle with ${this.color} color`)
+    }
+}
+
+const rec1 = new rectangle(2,4,"blue")
+rec1.paint()
+const area1 = rec1.area()
+console.log(area1);
