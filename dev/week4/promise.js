bdcb5145eb470c454b9fa211cbfbf859470b0fd3
dev2kod <devesh4coding@gmail.com>
2026-02-24 23:36:48 +0530

    starting off async await

--- dev/week4/promise.js
+++ dev/week4/promise.js
@@ -1,1 +1,13 @@
- 
+let 
+const p = new Promise((resolve,reject)=>{
+    console.log("promise") 
+ });
+p.then(console.log("done hai bahi"))
+p.catch(console.log("error")) 
+
+// promisified read file
+
+const readFilePromisified = () =>{
+    return new Promise((resolve,reject)=>{
+    }).then(console.log("dev"))
+}
