ae39b7618f46b8cfca9068c41a7ad352662cecc3
dev2kod <devesh4coding@gmail.com>
2026-03-06 11:22:31 +0530

    switching to project

---
+++ dev/week6/auth-app/backend/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken")
+const secret = "1234567890"
+const checkJwt = (req,res,next)=>{
+    const token = req.headers.Authorization;
+    if(!token){
+        res.status(403).send({
+            msg : "unauthorized"
+        })
+        return;
+    }
+
+    const user = jwt.verify(token,secret);
+    if(!user){
+        res.status(403).send({
+            msg : "unauthorized"
+        })
+        return;
+    }
+    req.user = user.username
+    next()
+}
