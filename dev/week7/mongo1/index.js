ac7babc2fb0fa020ee5443ce67a7c64e4b99917d
dev2kod <devesh4coding@gmail.com>
2026-03-30 22:36:02 +0530

    changes

--- dev/week7/mongo1/index.js
+++ dev/week7/mongo1/index.js
@@ -5,54 +5,78 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 const app = express()
-
+app.use(express.json())
 connectdb();
 
-app.get("/", (req, res) => {
-    res.send("Hello World")
+app.get("/",(req,res)=>{
+    res.json({
+        msg:"server running"
+    })
 })
 
-//signup
-/*
-1. get the data from req.body
-2. check if user already exists or not
-3. if exists send error response
-4. if not create the user in db and send success response
-const name = req.body.name  
-const email = req.body.email
-const password = req.body.password
-userModel.findOne({email:email},(err,user)=>{
-    if(err){
-        res.send("something went wrong")
-    } else if(user){
-        res.send("user already exists")
-    } else {
-        const newUser = new userModel({name,email,password})
-        newUser.save()
-        res.send("user created successfully")
+app.post("/signup",async(req,res)=>{
+    const {name,email,password} = req.body;
+    
+    const userExist = await userModel.findOne({
+        email: email
+    })
+
+    if(userExist){
+        res.status(400).json({
+            msg: "user already exists"
+        })
+        return;
+    }
+
+    await userModel.create({
+        name: name,
+        email: email,
+        password: password
+    })
+
+    res.json({
+        msg: "user signed up"
+    })
+})
+const jwt = require("jsonwebtoken")
+
+const JWT_SECRET = "123456"
+
+app.post("/signin", async (req,res)=>{
+    const {email,password} = req.body
+
+    try {
+
+        const user = await userModel.findOne({ email })
+
+        if(!user){
+            return res.status(404).json({
+                msg:"User does not exist"
+            })
+        }
+
+        if(user.password !== password){
+            return res.status(403).json({
+                msg:"Wrong password"
+            })
+        }
+
+        const token = jwt.sign(
+            { id: user._id },
+            JWT_SECRET
+        )
+
+        res.json({
+            msg:"User signed in",
+            token: token
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            msg:"Server error"
+        })        
     }
 })
-*/
-
-app.post("/signup",(req,res)=>{
-})
-
-app.post("/signin",(req,res)=>{
-})
-app.get("/todos",(req,res)=>{
-})
-
-app.post("/todo",(req,res)=>{
-})
-app.get("/getAllTodos",(req,res)=>{
-
-})
-
-app.post("/edittodos",(req,res)=>{
-    
-})
-
-
 app.listen(3000,()=>{
     console.log("running on port 3000")
 })
