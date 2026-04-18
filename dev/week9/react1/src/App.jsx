cb28b877174c9ca7764c3b943dc97a11b4d2bfd4
dev2kod <devesh4coding@gmail.com>
2026-04-18 23:52:49 +0530

    changing contact details

--- dev/week9/react1/src/App.jsx
+++ dev/week9/react1/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-  import './App.css'
+import './App.css'
+import Profile from './components/Profile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <><div className='flex items-center justify-center'>
+    <>
+    <Profile/>
+    <div className='flex items-center justify-center'>
       <div className='font-extrabold bg-amber-200 size-48 flex justify-center text-center'>
         Hi there
-        
+
       </div>
       </div>
     </>
