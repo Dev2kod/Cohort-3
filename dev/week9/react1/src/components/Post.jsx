041a391c680d0babf104d9bb92fdf8aa2f6b4ccc
dev2kod <devesh4coding@gmail.com>
2026-05-01 23:52:45 +0530

    react post creation

--- dev/week9/react1/src/components/Post.jsx
+++ dev/week9/react1/src/components/Post.jsx
@@ -1,17 +1,1 @@
-import React from 'react'
-import react from '../assets/react.svg'
-
-const Post = () => {
-  return (
-
-    <div className='flex flex-col w-full bg-white rounded-lg shadow-lg p-4 m-4 border-b-gray-800'>         
-        <div className='flex items-center'>
-            <div className=''>
-                <img src={react} alt="" />
-            </div>
-        </div>
-    </div>
-  )
-}
-
-export default Post 
+post
