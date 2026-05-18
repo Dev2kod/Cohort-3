2677f96069f28b9a4a48880e4d0fa647c313f960
Dev <113640546+Dev2kod@users.noreply.github.com>
2026-05-18 06:03:17 +0000

    arrays1

---
+++ dev/week4/apnaClg/async.js
@@ -0,0 +1,11 @@
+const greet=()=>{
+    console.log("hery there")
+}
+
+console.log("1")
+console.log("2")
+
+setTimeout(()=>greet(), 2000);
+
+console.log("3")
+console.log("4")
--- dsa/arrays/array.cpp
+++ dsa/arrays/array.cpp
@@ -1,2 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+int main(){ 
+
+    int arr [100];
+    
+    cout<<arr[9]<<endl;
+    
+}
