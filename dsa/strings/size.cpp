bf6f9c2a337b4121a9bb491d05315c60a3847dc4
dev2kod <devesh4coding@gmail.com>
2026-05-24 23:13:47 +0530

    string 2

---
+++ dsa/strings/size.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string a = "devesh";
+    int size = 0;
+    int i = 0;
+    while(a[i] != '\0'){
+        size++;
+        i++;
+    }
+    cout<<size;
+}
--- dsa/strings/string2.cpp
+++ dsa/strings/string2.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 
 int main(){
-string a = "devesh";
 
-cout<<a[0];
-
+    string a = "devesh";
+    cout<<a[0];
 
 }
