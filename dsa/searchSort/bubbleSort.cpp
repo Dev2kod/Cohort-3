c7017941e911889325a2219429118dd890c1daa0
Dev <113640546+Dev2kod@users.noreply.github.com>
2026-06-05 10:06:01 +0000

    sorts

--- dsa/searchSort/bubbleSort.cpp
+++ dsa/searchSort/bubbleSort.cpp
@@ -2,30 +2,10 @@
 using namespace std;
 
 void sort(int arr[],int n){
-    for (int i = 0; i < n; i++)
-    {
-        int max = 0;
-        int index = 0;
-        for (int j = 0; j <n-i ; j++)
-        {
-            if(arr[j]>max){
-                index = j;
-                max = arr[j];
-            }
-        }
-        swap(arr[index],arr[n-1-i]);
-    }
-    
+
 }
 
 int main(){
 
-    int arr[] = {0,9,8,7,6,5,4};    
-    sort(arr,7);
-
-    for (int i = 0; i < 7; i++)
-    {
-        cout<<arr[i]<<" ";
-    }
     
 }
