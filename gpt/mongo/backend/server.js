a3b8d52724b1e086ce8622b34352670c9c0ccca5
dev2kod <devesh4coding@gmail.com>
2026-06-29 23:54:29 +0530

    motion frontend

--- gpt/mongo/backend/server.js
+++ gpt/mongo/backend/server.js
@@ -14,12 +14,17 @@ app.get("/",(req,res)=>{
 })
 
 app.post("/employees",async(req,res)=>{
-    const data = req.body;
-    const employee = new Employee(data)
-
-    await employee.save()
     try {
+    
+        const data = req.body;
+        const employee = new Employee(data)
+
+        await employee.save()
         
+        res.status(200).json({
+            success:true,
+            message:"employee created"
+        })
     } catch (error) {
         res.status(500).json({
             error: error.message
